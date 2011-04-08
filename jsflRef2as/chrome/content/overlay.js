039945360e8d2b79a169a81ceaf08ba3272a6c2c
tamt <tamt@igg.com>
2011-04-08 18:32:06 +0800

    addon for extract AS3 class from JSFL references

--- jsflRef2as/chrome/content/overlay.js
+++ jsflRef2as/chrome/content/overlay.js
@@ -1,21 +1,78 @@
+﻿jQuery.noConflict();
+$ = function(selector,context) { 
+    return new jQuery.fn.init(selector,context); 
+};
+$.fn = $.prototype = jQuery.fn;
+
 var asjsfl = {
   onLoad: function() {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("asjsfl-strings");
+    
+    //侦听页面内容加载完成的事件
+    gBrowser.addEventListener("DOMContentLoaded", delay, true);
   },
+  
+  //注入jQuery
+  injectJQ:function(doc,aEvent){
+		// Check for website
+		if (!doc.location.href.match(/^http:\/\/(.*\.)?adobe\.com(\/.*)?$/i))  
+		    return;
+		
+		// Check if already loaded
+		if (doc.getElementById("plugin-example")) return;
+		
+		// Setup
+		this.win = aEvent.target.defaultView.wrappedJSObject;
+		this.doc = doc;
+		
+		// Hello World
+		this.main = main = $('<div id="plugin-example">').appendTo(doc.body).html('Example Loaded!');
+		main.css({ 
+		    background:'#FFF',color:'#000',position:'absolute',top:0,left:0,padding:8
+		});
+		main.html(main.html() + ' - jQuery <b>' + $.fn.jquery + '</b>');
+	},
 
   onMenuItemCommand: function(e) {
+	this.win = window._content.document;
     var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
                                   .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
+    
+    var _prefService = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+    var exportPath = _prefService.getComplexValue("extensions.asjsfl.exportPath", Components.interfaces.nsISupportsString).data;
+    
+    if($("h1", this.win)){
+	    var h1Str =jQuery.trim($("h1", this.win).text());
+	    var className = h1Str.substring(0, h1Str.lastIndexOf("对象") - 1);
+		var eles = $("div .tablenoborder table td", this.win);
+		for(var i=0; i<eles.length; i++){
+			//alert($(eles[i], this.win).text());
+		}
+
+		//输出到as
+    	var as3File = fInspectorFileIO.open(exportPath + "\\" + className + ".as");
+    	if(!as3File.exists()){
+    		fInspectorFileIO.create(as3File);
+    	}else{
+    		alert("exists!!");
+    	}
+    }
+	
   },
 
   onToolbarButtonCommand: function(e) {
     // just reuse the function above.  you can change this, obviously!
     asjsfl.onMenuItemCommand(e);
   }
+};
+
+var delay = function(aEvent) { 
+    var doc = aEvent.originalTarget;
+    setTimeout(function() { 
+    	asjsfl.injectJQ(doc,aEvent); 
+    }, 1); 
 };
 
 window.addEventListener("load", function () { asjsfl.onLoad(); }, false);
--- jsflRef2as/defaults/preferences/prefs.js
+++ jsflRef2as/defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 pref("extensions.asjsfl.boolpref", false);
 pref("extensions.asjsfl.intpref", 0);
-pref("extensions.asjsfl.stringpref", "A string");
+pref("extensions.asjsfl.exportPath", "E:\flash\git\asjsfl\src\jsfl");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.asjsfl@itamt.com.description", "chrome://asjsfl/locale/overlay.properties");
